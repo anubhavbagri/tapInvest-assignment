d1142aaecb433842b93b231afc2c58ce69617ed1
anubhavbagri <anubhavbagri01@gmail.com>
2024-02-11 16:54:52 +0530

    feat: add appbar & properties

--- lib/layers/presentation/app_root.dart
+++ lib/layers/presentation/app_root.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tap_invest/layers/presentation/constants/app_strings.dart';
 import 'package:tap_invest/layers/presentation/theme.dart';
+import 'package:tap_invest/layers/presentation/utils/size_config.dart';
 
 class AppRoot extends StatelessWidget {
   const AppRoot({super.key});
@@ -7,7 +9,7 @@ class AppRoot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const theme = CustomTheme();
-    // SizeConfig().init(context);
+    SizeConfig().init(context);
 
     return MaterialApp(
       theme: theme.base(),
@@ -15,16 +17,34 @@ class AppRoot extends StatelessWidget {
       title: 'tapInvest',
       home: Builder(
         builder: (context) {
-          // final tt = Theme.of(context).textTheme;
-          // final cs = Theme.of(context).colorScheme;
           return Scaffold(
-            extendBodyBehindAppBar: true,
             appBar: AppBar(
-                title: Text(
-              'data',
-              style: TextStyle(fontSize: 50, color: CustomTheme.primaryColor),
-            )),
-            body: Container(),
+              leading: IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.arrow_back_rounded,
+                  color: CustomTheme.green700,
+                ),
+                padding: EdgeInsets.only(
+                  left: CustomTheme.paddingHorizontal,
+                ),
+              ),
+              toolbarHeight: SizeConfig.safeVertical! * 0.1,
+              // backgroundColor: Colors.black26,
+              // leadingWidth: SizeConfig.safeHorizontal! * 0.2,
+              title: Text(
+                AppStrings.appbarText,
+                style: CustomTheme.h6(context),
+              ),
+              centerTitle: false,
+              automaticallyImplyLeading: false,
+              titleSpacing: 0,
+            ),
+            body: Center(child: Container()),
+            bottomNavigationBar: Container(
+              color: Colors.black54,
+              height: 30,
+            ),
           );
         },
       ),
--- lib/layers/presentation/theme.dart
+++ lib/layers/presentation/theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tap_invest/layers/presentation/utils/hex_color.dart';
+import 'package:tap_invest/layers/presentation/utils/size_config.dart';
 
 @immutable
 class CustomTheme extends ThemeExtension<CustomTheme> {
@@ -9,6 +11,106 @@ class CustomTheme extends ThemeExtension<CustomTheme> {
   static final primaryColor = HexColor("#16A34A");
   static final tertiaryColor = HexColor("15803D");
   static final neutralColor = HexColor("D6D3D1");
+  static final green700 = HexColor("#15803D");
+
+  static final double paddingVertical = SizeConfig.safeVertical! * .02;
+  static final double paddingHorizontal = SizeConfig.safeHorizontal! * .04;
+  static final double paddingRight = SizeConfig.safeHorizontal! * .08;
+  static final double paddingAllSides = SizeConfig.safeHorizontal! * .03;
+  static final double textPadding = SizeConfig.safeHorizontal! * .02;
+
+  /// headline4 - headlineMedium
+  static TextStyle h4(
+      BuildContext context, {
+        Color? color,
+        FontWeight? weight,
+        double? letterSpacing,
+      }) {
+    return Theme.of(context).textTheme.headlineMedium!.copyWith(
+      fontFamily: GoogleFonts.inter(fontWeight: weight ?? FontWeight.w600)
+          .fontFamily,
+      letterSpacing: letterSpacing,
+      color: color ?? green700,
+    );
+  }
+
+  /// headline5 - headlineSmall
+  static TextStyle h5(
+      BuildContext context, {
+        Color? color,
+        FontWeight? weight,
+      }) {
+    return Theme.of(context).textTheme.headlineSmall!.copyWith(
+      fontFamily: GoogleFonts.inter(fontWeight: weight ?? FontWeight.w600)
+          .fontFamily,
+      color: color ?? green700,
+    );
+  }
+
+  /// headline6 - titleLarge
+  static TextStyle h6(
+      BuildContext context, {
+        Color? color,
+        FontWeight? weight,
+      }) {
+    return Theme.of(context).textTheme.titleLarge!.copyWith(
+      fontFamily: GoogleFonts.inter(fontWeight: weight ?? FontWeight.w500)
+          .fontFamily,
+      color: color ?? green700,
+    );
+  }
+
+  /// subtitle1 - titleMedium
+  static TextStyle s1(
+      BuildContext context, {
+        Color? color,
+        FontWeight? weight,
+      }) {
+    return Theme.of(context).textTheme.titleMedium!.copyWith(
+      fontFamily: GoogleFonts.inter(fontWeight: weight ?? FontWeight.w600)
+          .fontFamily,
+      color: color ?? green700,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  // subtitle2 - titleSmall
+  static TextStyle s2(
+      BuildContext context, {
+        Color? color,
+        FontWeight? weight,
+        TextDecoration? decoration,
+        bool isOverflow = true,
+      }) {
+    return Theme.of(context).textTheme.titleSmall!.copyWith(
+      fontFamily: GoogleFonts.inter(fontWeight: weight ?? FontWeight.w500)
+          .fontFamily,
+      color: color ?? green700,
+      decoration: decoration,
+      overflow: isOverflow ? TextOverflow.ellipsis : TextOverflow.visible,
+    );
+  }
+
+  /// bodySmall - bodySmall
+  static TextStyle bs(
+      BuildContext context, {
+        Color? color,
+        FontWeight? weight,
+        TextDecoration? decoration,
+        double? letterSpacing,
+        double? fontSize,
+        bool isOverflow = true,
+      }) {
+    return Theme.of(context).textTheme.bodySmall!.copyWith(
+      fontFamily: GoogleFonts.inter(fontWeight: weight ?? FontWeight.w500)
+          .fontFamily,
+      color: color ?? green700,
+      decoration: decoration,
+      letterSpacing: letterSpacing,
+      fontSize: fontSize,
+      overflow: isOverflow ? TextOverflow.ellipsis : TextOverflow.visible,
+    );
+  }
 
   ThemeData base() {
     final primaryTextTheme = GoogleFonts.interTextTheme();
@@ -27,10 +129,14 @@ class CustomTheme extends ThemeExtension<CustomTheme> {
       extensions: [this],
       visualDensity: VisualDensity.adaptivePlatformDensity,
       textTheme: textTheme,
-      // appBarTheme: AppBarTheme(
-      // ),
-      cardTheme: CardTheme(color: tertiaryColor),
-      // scaffoldBackgroundColor: isLight ? neutralColor : colorScheme.background,
+      appBarTheme: const AppBarTheme(
+        color: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle:
+            SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+      ),
+      // scaffoldBackgroundColor: Colors.white,
+      // cardTheme: CardTheme(color: tertiaryColor),
       // tabBarTheme: TabBarTheme(
       //     labelColor: colorScheme.onSurface,
       //     unselectedLabelColor: colorScheme.onSurface,
