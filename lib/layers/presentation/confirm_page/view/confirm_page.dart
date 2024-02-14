19d7992660a11c8c92175de622604151a5c8efe1
anubhavbagri <anubhavbagri01@gmail.com>
2024-02-14 06:43:07 +0530

    wip: lottie animation remaining

--- lib/layers/presentation/confirm_page/view/confirm_page.dart
+++ lib/layers/presentation/confirm_page/view/confirm_page.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:tap_invest/layers/presentation/constants/app_strings.dart';
+import 'package:tap_invest/layers/presentation/theme.dart';
+import 'package:tap_invest/layers/presentation/utils/hex_color.dart';
+import 'package:tap_invest/layers/presentation/utils/size_config.dart';
 
 class ConfirmPage extends StatelessWidget {
   const ConfirmPage({Key? key}) : super(key: key);
 
   static Route<void> route() {
     return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) {
-        /// can initialize page level cubit here with parameter value
-        return const ConfirmPage();
-      },
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          const ConfirmPage(),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        // const begin = Offset(0.0, 1.0);
-        // const end = Offset.zero;
-        // final tween = Tween(begin: begin, end: end);
-        // final offsetAnimation = animation.drive(tween);
-        // return SlideTransition(
-        //   position: offsetAnimation,
-        //   child: child,
-        // );
         return FadeTransition(
           opacity: animation,
           child: child,
@@ -30,11 +24,100 @@ class ConfirmPage extends StatelessWidget {
   Widget build(BuildContext context) => const ConfirmView();
 }
 
-class ConfirmView extends StatelessWidget {
+class ConfirmView extends StatefulWidget {
   const ConfirmView({Key? key}) : super(key: key);
 
   @override
+  State<ConfirmView> createState() => _ConfirmViewState();
+}
+
+class _ConfirmViewState extends State<ConfirmView> {
+  bool _isVisible = false;
+  bool _isIconVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _isVisible = true;
+        Future.delayed(const Duration(milliseconds: 1200), () {
+          setState(() {
+            _isIconVisible = true;
+          });
+        });
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    SizeConfig().init(context);
+
+    return Scaffold(
+      backgroundColor: CustomTheme.green700,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: SizeConfig.safeVertical! * .2,
+            ),
+            Center(
+              child: AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(seconds: 1),
+                curve: Curves.easeInExpo,
+                child: Container(
+                  height: SizeConfig.safeVertical! * .12,
+                  width: SizeConfig.safeVertical! * .12,
+                  decoration: BoxDecoration(
+                    color: HexColor("#116631"),
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  child: AnimatedOpacity(
+                    opacity: _isIconVisible ? 1.0 : 0.0,
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.fastOutSlowIn,
+                    child: const Icon(
+                      Icons.check_circle_rounded,
+                      color: Colors.white,
+                      size: 40,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            vSizedBox5,
+            AnimatedOpacity(
+              opacity: _isIconVisible ? 1.0 : 0.0,
+              duration: const Duration(seconds: 1),
+              curve: Curves.decelerate,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Payment done',
+                    textAlign: TextAlign.center,
+                    style: CustomTheme.h6(context, color: Colors.white),
+                  ),
+                  vSizedBox3,
+                  Text(
+                    AppStrings.confirmDesc,
+                    textAlign: TextAlign.center,
+                    style: CustomTheme.s2(
+                      context,
+                      color: Colors.white,
+                      weight: FontWeight.w400,
+                      isOverflow: false,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/layers/presentation/constants/app_strings.dart
+++ lib/layers/presentation/constants/app_strings.dart
@@ -2,8 +2,12 @@ class AppStrings {
   const AppStrings._();
 
   static const appbarText = 'Back to Deals';
-  static const agrizyDesc = 'Agrizy offers solutions across digital vendor management, and supply and value chainautomation to its agri processing units. Agrizy, a Bengaluru-based agri tech startup.';
-  static const highlightDesc = 'Agrizy was founded in 2021 by Vicky Dodani and Saket Chirania to provide an end-to-end solution to the agri processing market.';
+  static const agrizyDesc =
+      'Agrizy offers solutions across digital vendor management, and supply and value chainautomation to its agri processing units. Agrizy, a Bengaluru-based agri tech startup.';
+  static const highlightDesc =
+      'Agrizy was founded in 2021 by Vicky Dodani and Saket Chirania to provide an end-to-end solution to the agri processing market.';
   static const tapToInvest = 'Tap to Invest';
   static const errorText = 'Please enter an amount.';
+  static const confirmDesc =
+      'You are almost there!\nDo not leave this page, or press the back button.';
 }
