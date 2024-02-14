b383502ec29b5fa8b614f742bdb017ccaf80e9a0
anubhavbagri <anubhavbagri01@gmail.com>
2024-02-14 08:21:48 +0530

    feat: lottie working successfully

--- lib/layers/presentation/confirm_page/view/confirm_page.dart
+++ lib/layers/presentation/confirm_page/view/confirm_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:tap_invest/layers/presentation/constants/app_asset_paths.dart';
 import 'package:tap_invest/layers/presentation/constants/app_strings.dart';
 import 'package:tap_invest/layers/presentation/theme.dart';
 import 'package:tap_invest/layers/presentation/utils/hex_color.dart';
@@ -31,9 +33,11 @@ class ConfirmView extends StatefulWidget {
   State<ConfirmView> createState() => _ConfirmViewState();
 }
 
-class _ConfirmViewState extends State<ConfirmView> {
+class _ConfirmViewState extends State<ConfirmView>
+    with TickerProviderStateMixin {
   bool _isVisible = false;
   bool _isIconVisible = false;
+  AnimationController? _animationController;
 
   @override
   void initState() {
@@ -49,6 +53,33 @@ class _ConfirmViewState extends State<ConfirmView> {
         });
       });
     });
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    Future.delayed(const Duration(seconds: 2), () => _playAnimationTwice());
+  }
+
+  void _playAnimationTwice() {
+    _animationController?.forward();
+    _animationController!.addStatusListener(_animationStatusListener);
+  }
+
+  void _animationStatusListener(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      _animationController?.forward(from: 0.0);
+    } else if (status == AnimationStatus.dismissed) {
+      _animationController?.removeStatusListener(_animationStatusListener);
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationController
+        ?.dispose(); // Dispose the AnimationController to avoid memory leaks
+    super.dispose();
   }
 
   @override
@@ -58,62 +89,85 @@ class _ConfirmViewState extends State<ConfirmView> {
     return Scaffold(
       backgroundColor: CustomTheme.green700,
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            SizedBox(
-              height: SizeConfig.safeVertical! * .2,
+            Image.asset(
+              AppAssetPath.vectorBg,
+              fit: BoxFit.fill,
+              width: double.infinity,
+              height: double.infinity,
             ),
-            Center(
-              child: AnimatedOpacity(
-                opacity: _isVisible ? 1.0 : 0.0,
-                duration: const Duration(seconds: 1),
-                curve: Curves.easeInExpo,
-                child: Container(
-                  height: SizeConfig.safeVertical! * .12,
-                  width: SizeConfig.safeVertical! * .12,
-                  decoration: BoxDecoration(
-                    color: HexColor("#116631"),
-                    borderRadius: BorderRadius.circular(14),
-                  ),
+            Lottie.asset(
+              AppAssetPath.dotLottie,
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.fill,
+              controller: _animationController,
+              // onLoaded: (composition) {
+              //   setState(() {
+              //     _animationController?.duration = composition.duration;
+              //   });
+              // },
+              // repeat: true,
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: SizeConfig.safeVertical! * .2,
+                ),
+                Center(
                   child: AnimatedOpacity(
-                    opacity: _isIconVisible ? 1.0 : 0.0,
+                    opacity: _isVisible ? 1.0 : 0.0,
                     duration: const Duration(seconds: 1),
-                    curve: Curves.fastOutSlowIn,
-                    child: const Icon(
-                      Icons.check_circle_rounded,
-                      color: Colors.white,
-                      size: 40,
+                    curve: Curves.easeInExpo,
+                    child: Container(
+                      height: SizeConfig.safeVertical! * .12,
+                      width: SizeConfig.safeVertical! * .12,
+                      decoration: BoxDecoration(
+                        color: HexColor("#116631"),
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      child: AnimatedOpacity(
+                        opacity: _isIconVisible ? 1.0 : 0.0,
+                        duration: const Duration(seconds: 1),
+                        curve: Curves.fastOutSlowIn,
+                        child: const Icon(
+                          Icons.check_circle_rounded,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            vSizedBox5,
-            AnimatedOpacity(
-              opacity: _isIconVisible ? 1.0 : 0.0,
-              duration: const Duration(seconds: 1),
-              curve: Curves.decelerate,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Payment done',
-                    textAlign: TextAlign.center,
-                    style: CustomTheme.h6(context, color: Colors.white),
+                vSizedBox5,
+                AnimatedOpacity(
+                  opacity: _isIconVisible ? 1.0 : 0.0,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.decelerate,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Payment done',
+                        textAlign: TextAlign.center,
+                        style: CustomTheme.h6(context, color: Colors.white),
+                      ),
+                      vSizedBox3,
+                      Text(
+                        AppStrings.confirmDesc,
+                        textAlign: TextAlign.center,
+                        style: CustomTheme.s2(
+                          context,
+                          color: Colors.white,
+                          weight: FontWeight.w400,
+                          isOverflow: false,
+                        ),
+                      ),
+                    ],
                   ),
-                  vSizedBox3,
-                  Text(
-                    AppStrings.confirmDesc,
-                    textAlign: TextAlign.center,
-                    style: CustomTheme.s2(
-                      context,
-                      color: Colors.white,
-                      weight: FontWeight.w400,
-                      isOverflow: false,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
--- lib/layers/presentation/constants/app_asset_paths.dart
+++ lib/layers/presentation/constants/app_asset_paths.dart
@@ -4,4 +4,6 @@ class AppAssetPath {
   // static const logo = 'assets/images/Logo.png';
   static const logo2 = 'assets/images/Logo2.png';
   static const google = 'assets/images/google.png';
+  static const vectorBg = 'assets/images/Vector.png';
+  static const dotLottie = 'assets/lottie/dot.json';
 }
