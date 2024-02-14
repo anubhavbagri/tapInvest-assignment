56a563313d07cc5adfc0969613ef199572b222c3
anubhavbagri <anubhavbagri01@gmail.com>
2024-02-14 20:33:21 +0530

    fix: breaking UI in release

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'layers/presentation/app_root.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  /// https://github.com/flutter/flutter/pull/104828
+  /// https://github.com/flutter/flutter/issues/104821#issuecomment-1139615118
+  /// temporary fix (code smell): https://github.com/flutter/flutter/issues/99680#issuecomment-1071148878
+  await Future.delayed(const Duration(milliseconds: 500));
   runApp(const AppRoot());
 }
