d2aa59764a98af17eb17dd8b88bac5eb3c2bc1c9
anubhavbagri <anubhavbagri01@gmail.com>
2024-02-14 08:47:08 +0530

    add new pg

--- lib/layers/presentation/confirm_page/view/generate_page.dart
+++ lib/layers/presentation/confirm_page/view/generate_page.dart
@@ -1,42 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:tap_invest/layers/presentation/confirm_page/view/generate_page.dart';
 import 'package:tap_invest/layers/presentation/constants/app_asset_paths.dart';
 import 'package:tap_invest/layers/presentation/constants/app_strings.dart';
 import 'package:tap_invest/layers/presentation/theme.dart';
 import 'package:tap_invest/layers/presentation/utils/hex_color.dart';
 import 'package:tap_invest/layers/presentation/utils/size_config.dart';
 
-class ConfirmPage extends StatelessWidget {
-  const ConfirmPage({Key? key}) : super(key: key);
+class GeneratePage extends StatelessWidget {
+  const GeneratePage({Key? key}) : super(key: key);
 
   static Route<void> route() {
     return PageRouteBuilder(
       pageBuilder: (context, animation, secondaryAnimation) =>
-          const ConfirmPage(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return FadeTransition(
-          opacity: animation,
-          child: child,
-        );
-      },
+          const GeneratePage(),
+      transitionDuration: Duration.zero,
+      reverseTransitionDuration: Duration.zero,
     );
   }
 
   @override
-  Widget build(BuildContext context) => const ConfirmView();
+  Widget build(BuildContext context) => const GenerateView();
 }
 
-class ConfirmView extends StatefulWidget {
-  const ConfirmView({Key? key}) : super(key: key);
+class GenerateView extends StatefulWidget {
+  const GenerateView({Key? key}) : super(key: key);
 
   @override
-  State<ConfirmView> createState() => _ConfirmViewState();
+  State<GenerateView> createState() => _GenerateViewState();
 }
 
-class _ConfirmViewState extends State<ConfirmView>
+class _GenerateViewState extends State<GenerateView>
     with TickerProviderStateMixin {
-  bool _isVisible = false;
   bool _isIconVisible = false;
   AnimationController? _animationController;
 
@@ -44,14 +38,9 @@ class _ConfirmViewState extends State<ConfirmView>
   void initState() {
     super.initState();
 
-    Future.delayed(const Duration(milliseconds: 500), () {
+    Future.delayed(const Duration(seconds: 1), () {
       setState(() {
-        _isVisible = true;
-        Future.delayed(const Duration(milliseconds: 1200), () {
-          setState(() {
-            _isIconVisible = true;
-          });
-        });
+        _isIconVisible = true;
       });
     });
 
@@ -73,8 +62,6 @@ class _ConfirmViewState extends State<ConfirmView>
       _animationController?.forward(from: 0.0);
     } else if (status == AnimationStatus.dismissed) {
       _animationController?.removeStatusListener(_animationStatusListener);
-      Future.delayed(const Duration(seconds: 1),
-          () => Navigator.of(context).push(GeneratePage.route()));
     }
   }
 
@@ -106,12 +93,6 @@ class _ConfirmViewState extends State<ConfirmView>
               height: double.infinity,
               fit: BoxFit.fill,
               controller: _animationController,
-              // onLoaded: (composition) {
-              //   setState(() {
-              //     _animationController?.duration = composition.duration;
-              //   });
-              // },
-              // repeat: true,
             ),
             Column(
               children: [
@@ -119,26 +100,21 @@ class _ConfirmViewState extends State<ConfirmView>
                   height: SizeConfig.safeVertical! * .2,
                 ),
                 Center(
-                  child: AnimatedOpacity(
-                    opacity: _isVisible ? 1.0 : 0.0,
-                    duration: const Duration(milliseconds: 600),
-                    curve: Curves.easeInExpo,
-                    child: Container(
-                      height: SizeConfig.safeVertical! * .12,
-                      width: SizeConfig.safeVertical! * .12,
-                      decoration: BoxDecoration(
-                        color: HexColor("#116631"),
-                        borderRadius: BorderRadius.circular(14),
-                      ),
-                      child: AnimatedOpacity(
-                        opacity: _isIconVisible ? 1.0 : 0.0,
-                        duration: const Duration(seconds: 1),
-                        curve: Curves.fastOutSlowIn,
-                        child: const Icon(
-                          Icons.check_circle_rounded,
-                          color: Colors.white,
-                          size: 40,
-                        ),
+                  child: Container(
+                    height: SizeConfig.safeVertical! * .12,
+                    width: SizeConfig.safeVertical! * .12,
+                    decoration: BoxDecoration(
+                      color: HexColor("#116631"),
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                    child: AnimatedOpacity(
+                      opacity: _isIconVisible ? 1.0 : 0.0,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.fastOutSlowIn,
+                      child: const Icon(
+                        Icons.my_library_books_outlined,
+                        color: Colors.white,
+                        size: 40,
                       ),
                     ),
                   ),
@@ -146,13 +122,13 @@ class _ConfirmViewState extends State<ConfirmView>
                 vSizedBox5,
                 AnimatedOpacity(
                   opacity: _isIconVisible ? 1.0 : 0.0,
-                  duration: const Duration(seconds: 1),
+                  duration: const Duration(milliseconds: 500),
                   curve: Curves.decelerate,
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Text(
-                        'Payment done',
+                        'Generating Contract',
                         textAlign: TextAlign.center,
                         style: CustomTheme.h6(context, color: Colors.white),
                       ),
